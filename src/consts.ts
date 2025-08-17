7cc31c003c56138deab049900244f40271f74dd7
Heipry <heipry@hotmail.com>
2025-08-17 18:11:38 +0200

    Personalización inicial

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog de Javier Díaz';
+export const SITE_TITLE = 'Javier Díaz Blog';
 export const SITE_DESCRIPTION = 'Blog sobre docencia de Javier Díaz, docente y desarrollador web.';
